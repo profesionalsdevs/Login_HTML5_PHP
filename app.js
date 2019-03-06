01592b05187289dc9b719c8781eed881efc1b9b6
Extibax <extibax@gmail.com>
2019-03-06 16:08:59 -0500

    Login is OK

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ $(document).ready(() => {
 
     $('#login_form').submit((e) => {
         const postData = {
-            Username: $('#username'),
-            Password: $('#password')
+            username: $('#username').val(),
+            password: $('#password').val()
         };
 
         $.post('PHP/login.php', postData, (response) => {
+
             $('#login_form').trigger('reset');
-            
+
             console.log(response);
 
-            /* if (response === 1) {
+            if (response == 1) {
                 window.location = 'home.html';
             }
             else {
                 console.log('Something is wrong');
-            } */
+            }
         });
         e.preventDefault();
     });
